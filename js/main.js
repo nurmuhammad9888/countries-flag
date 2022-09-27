f0992eb94813390ae52df544df4351f3e68a5325
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-09-27 18:00:38 +0500

    Add new

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 let elDark = document.querySelector(".sitenav__link");
 let elBody = document.querySelector("body");
 let elBack = document.querySelector(".hero-main__arrow");
+let elLabel = document.querySelector(".hero_label");
+let elsectIcon = document.querySelector(".hero__select")
 
 elDark.addEventListener("click" , function (){
     elBody.classList.toggle("dark")
+    elLabel.classList.toggle("hero_label-show")
+    elsectIcon.classList.toggle("hero__select-show")
+})
+elDark.addEventListener("click" , function (){
     elBack.classList.toggle("hero-main__arrow-show")
 })
